c44cf7420b8b1e40fbe0ba8ba6517a156f561528
Divita Pal <divitapal@gmail.com>
2018-11-15 10:24:49 -0800

    shiny app in class participation 8

--- bcl/app.R
+++ bcl/app.R
@@ -8,6 +8,7 @@
 #
 
 library(shiny)
+library(tidyverse)
 
 
 
@@ -32,7 +33,13 @@ ui <- fluidPage(
     titlePanel("BC Liquor price app", 
                windowTitle = "BCL app"),
     sidebarLayout(
-        sidebarPanel("This text is in the sidebar."),
+        sidebarPanel(
+            sliderInput("priceInput", "Select your desired price range.",
+                  min = 0, max = 100, value = c(15, 30), pre="$"),
+            radioButtons("typeInput", "Select your desired beverage type",
+                         choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+                         selected = "WINE")
+        ),
         mainPanel(
             plotOutput("price_hist"),
             tableOutput("price_table")
@@ -45,10 +52,24 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    output$price_hist <- renderPlot(ggplot2::qplot(bcl$Price))
-    output$price_table <- renderTable(bcl)
+    
+    observe(print(input$priceInout))
+    bcl_filtered <- reactive({
+        bcl %>% 
+        filter( Price < input$priceInput[2],
+                Price > input$priceInput[1],
+                Type == input$typeInput)
+    })
+    output$price_hist <- renderPlot({
+        bcl_filtered() %>% 
+            ggplot(aes(Price)) +
+            geom_histogram()
+        })
+    output$price_table <- renderTable({
+        bcl_filtered()   
+        }) 
    
-}
+} # curly brackets can be used with render function to run multiple lines of code.
 
 # Run the application 
 shinyApp(ui = ui, server = server)
